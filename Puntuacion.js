e90c36e956d1e2231d18a41e1e9db5601780aa42
AbelVelarde <AbelVelarde@users.noreply.github.com>
2019-07-31 10:09:29 +0200

    Snake

--- Puntuacion.js
+++ Puntuacion.js
@@ -1,4 +1,4 @@
-let puntuacionMaxima = '0';
+let puntuacionMaxima = getMaxPoints();
 let puntuacionAnterior = '0';
 let puntuacionActual = '0';
 
@@ -19,16 +19,46 @@ function actualizarPuntuacion() {
 function guardarPuntuaciones() {
     puntuacionAnterior = puntuacionActual;
     document.getElementById('pAnterior').value = puntuacionAnterior;
-    puntuacionActual > puntuacionMaxima ? guardarMaxima() : false;
+    puntuacionActual > puntuacionMaxima ? obtenerMaxPoints() : false;
     puntuacionActual = '0';
     document.getElementById('pActual').value = puntuacionActual;
-
-
 }
 
-function guardarMaxima() {
-    puntuacionMaxima = puntuacionActual;
-    localStorage.setItem('puntuacion', puntuacionMaxima);
-    document.getElementById('pMaxima').value= localStorage.getItem('puntuacion');
+function guardarMaxima(e, nombreJugador) {
+    e.maxPoints.nombreJugador = nombreJugador;
+    e.maxPoints.puntos = puntuacionActual;    
+}
+
+function obtenerMaxPoints(){
+
+    let nombreJugador = prompt("Has lograda la maxima puntuacion, introduce tu nombre");
+
+    let url = 'https://abelvelarde.github.io/maxPoints.json';
+
+    let request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.responseType = 'json';
+    request.send();
     
+    request.onload = function(){
+        puntuacionMaxima = request.response.puntos;
+    }
+}
+
+function getMaxPoints(){
+    let url = 'https://abelvelarde.github.io/maxPoints.json';
+    
+    let maxPoints = 0;
+
+    let request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.responseType = 'json';
+    request.send();
+    
+    request.onload = function(){
+        request.response.puntos = puntuacionActual;
+        request.nombreJugador = nombreJugador;
+    }
+
+    return maxPoints;
 }
