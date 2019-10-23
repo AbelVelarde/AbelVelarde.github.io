22b99e5823d079a20165ba9f1ef242512f48de75
Abel <abelvelardegarcia@gmail.com>
2019-10-23 18:57:14 +0200

    cambio de web service a Stephan Meals

---
+++ checkBoxController.js
@@ -0,0 +1,12 @@
+'use strict'
+
+function checkBoxControl(n) {
+    let categoria = document.getElementById('categoria');
+    let zona = document.getElementById('area');
+    let ingrediente = document.getElementById('ingrediente');
+    let name = document.getElementById('name');
+
+
+
+    
+}
